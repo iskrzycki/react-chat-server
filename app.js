f473973b4bda479c65f63ab2d0a7c62a94447107
Rafal Iskrzycki <rafal.iskrzycki@mobica.com>
2018-07-27 10:52:41 +0200

    Room constructor. Fixed bug, which was causing sending message to all sockets

--- app.js
+++ app.js
@@ -7,26 +7,44 @@ var port = 3000;
 
 var chats = [];
 
+// TODO: redesign Room constructor
+function Room(name, client) {
+    this.name = name;
+    this.client = client;
+}
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+// function returns chat Room by chatter's socket.id
+function getRoomByGuest(guest) {
+    console.log('AVAILABLE CHATS: ', chats);
+
+    for (let i = 0, len = chats.length; i < len; i++) {
+        console.log('Im in for function, name: ' + chats[i].name + ', client: ' + chats[i].client);
+        if (chats[i].client === guest || chats[i].host === guest) {
+            return chats[i];
+        }
+    }
+    return undefined;
+}
+
 io.on('connection', function (socket) {
 
     socket.on('SEND_MESSAGE', function (payload) {
-        console.log('NEW MESSAGE!!!!! ', payload);
-        io.emit('MESSAGE', payload);
+        let foundRoom = getRoomByGuest(socket.id);
+        console.log('New message: ' + payload.text + ', FROM: ' + socket.id + ', will be sent to room name: ' + foundRoom.name);
+        io.to(foundRoom.name).emit('MESSAGE', payload);
     });
 
     socket.on('GET_CHATS', function () {
-        console.log('GET_CHATS. Len: ', chats.length);
-
         io.emit('CHATS', chats);
     });
 
     socket.on('createChat', function () {
-        chats.push(socket.id);
         var chatName = 'ROOM ' + socket.id;
+        chats.push(new Room(chatName, socket.id));
         console.log('Chat ' + chatName + ' created');
 
         socket.join(chatName);
@@ -37,18 +55,21 @@ io.on('connection', function (socket) {
     socket.on('joinChat', function (chatName) {
         console.log('ADMIN joined ' + chatName);
         socket.join(chatName);
-        io.to(chatName.replace('ROOM ', '')).emit('MESSAGE', { who: 'host', text: 'Hello, lets chat!' });
+        let clientId = chatName.replace('ROOM ', '');
+        let foundRoom = getRoomByGuest(clientId);
+        if (foundRoom) {
+            foundRoom.host = socket.id;
+            io.to(clientId).emit('MESSAGE', { who: 'host', text: 'Hello, lets chat!' });
+        }
     });
 
     socket.on('disconnect', function () {
         console.log('USER ' + socket.id + ' disconnected');
-
-        var index = chats.indexOf(socket.id);
-        if (index !== -1) {
-            chats.splice(index, 1);
-        }
+        // removing room when client disconnected
+        chats = chats.filter(el => el.client !== socket.id || el.host !== socket.id);
 
         io.emit('CHATS', chats);
+        // TODO: Inform other guy, that user was disconnected from the room
     });
 });
 
